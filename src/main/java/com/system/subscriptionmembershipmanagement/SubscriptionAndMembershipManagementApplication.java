4010f9b392456051d1f06b1127b56642951776ce
Abdel Rahman <hossamabdelrahman62@gmail.com>
2025-08-02 20:43:51 +0300

    init project

---
+++ src/main/java/com/system/subscriptionmembershipmanagement/SubscriptionAndMembershipManagementApplication.java
@@ -0,0 +1,13 @@
+package com.system.subscriptionmembershipmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionAndMembershipManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriptionAndMembershipManagementApplication.class, args);
+    }
+
+}
