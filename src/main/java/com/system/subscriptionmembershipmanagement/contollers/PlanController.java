86d360741e0ae634f9bfa41e185c55d289278595
Abdel Rahman <hossamabdelrahman62@gmail.com>
2025-08-03 09:47:23 +0300

    add swagger config and anotation

--- src/main/java/com/system/subscriptionmembershipmanagement/contollers/PlanController.java
+++ src/main/java/com/system/subscriptionmembershipmanagement/contollers/PlanController.java
@@ -4,6 +4,9 @@ import com.system.subscriptionmembershipmanagement.dtos.CreatePlanRequest;
 import com.system.subscriptionmembershipmanagement.dtos.UpdatePlanRequest;
 import com.system.subscriptionmembershipmanagement.services.PlanService;
 import com.system.subscriptionmembershipmanagement.utils.ResponseUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,16 +18,26 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/plans")
 @PreAuthorize("hasRole('ADMIN')")
 @AllArgsConstructor
+@Tag(name = "Plans", description = "Endpoints for managing Plans plans (Admin only)")
+@SecurityRequirement(name = "bearerAuth")
 public class PlanController {
     private final PlanService planService;
 
     @PostMapping()
+    @Operation(
+            summary = "Create a new subscription plan",
+            description = "Creates a new subscription plan. Only accessible to admins."
+    )
     public ResponseEntity<?> createPlan(@Valid @RequestBody CreatePlanRequest request) {
         var response = planService.createPlan(request);
         return ResponseUtil.success("Created Plan Successfully", response, HttpStatus.CREATED);
     }
 
     @PatchMapping("/{id}")
+    @Operation(
+            summary = "Update a subscription plan",
+            description = "Updates an existing subscription plan by ID. Only accessible to admins."
+    )
     public ResponseEntity<?> updatePlan(@PathVariable(name = "id") Long planId, @Valid @RequestBody UpdatePlanRequest request) {
         var response = planService.updatePlan(planId, request);
         return ResponseUtil.success("Updated Plan Successfully", response);
@@ -32,6 +45,10 @@ public class PlanController {
 
 
     @DeleteMapping("/{id}")
+    @Operation(
+            summary = "Delete a subscription plan",
+            description = "Deletes a subscription plan by ID. Only accessible to admins."
+    )
     public ResponseEntity<?> deletePlan(@PathVariable(name = "id") Long planId) {
         planService.deletePlan(planId);
         return ResponseUtil.success("deleted Plan Successfully", planId);
